a9e57664bac476452cee4d950940561a01771cfc
hzuo <someotherusername@gmail.com>
2022-05-17 02:26:19 -0400

    simplify quickstart

--- quickstart/webpack.config.ts
+++ quickstart/webpack.config.ts
@@ -1,41 +1,17 @@
 import * as path from "path";
-import * as fs from "fs";
 import type { Configuration } from "webpack";
-
-const entrypointsDir = path.resolve(__dirname, "src", "workers");
-
-const findEntrypoints = (): Record<string, string> => {
-  const files = fs.readdirSync(entrypointsDir, {
-    encoding: "utf8",
-    withFileTypes: true,
-  });
-  const entrypoints: Record<string, string> = {};
-  for (const file of files) {
-    if (
-      file.isFile() &&
-      (file.name.endsWith(".ts") || file.name.endsWith(".js"))
-    ) {
-      const noExt = path.parse(file.name).name;
-      entrypoints[noExt] = path.resolve(entrypointsDir, file.name);
-    }
-  }
-  return entrypoints;
-};
-
-const entrypoints = findEntrypoints();
-console.log("Found webpack entrypoints:");
-console.log(JSON.stringify(entrypoints, null, 2));
 
 const config: Configuration = {
   mode: "production",
   target: "web",
-  entry: entrypoints,
+  entry: {
+    hello: "./src/hello.ts",
+  },
   module: {
     rules: [
       {
-        test: (f) => f.endsWith(".ts"),
+        test: /\.ts$/,
         use: "ts-loader",
-        exclude: (f) => f.includes("node_modules"),
       },
     ],
   },
