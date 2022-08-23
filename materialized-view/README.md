ca25d0696f7d6973ddeaebc3653d16c8055723ed
Arthur Wu <5306113+arthurwuhoo@users.noreply.github.com>
2022-08-23 13:37:24 -0400

    Init

---
+++ materialized-view/README.md
@@ -0,0 +1,16 @@
+# Overview
+
+Define a custom SQL query that generates a materialized view as an output table every 30 seconds. This modules is helpful to create tables with custom joins using the other tables in your Dataland workspace.
+
+It's helpful to test SQL queries in the Dataland CLI with the command `dataland sql`, before using the query in this module.
+
+`SELECT` statements are all allowed - even complex queries. See [the docs](https://docs.dataland.io/guides/querying-workspace-in-sql.html) for more detail.
+
+## Parameter setup
+
+| Name                                     | About                                                                                                                                       |
+| ---------------------------------------- | ------------------------------------------------------------------------------------------------------------------------------------------- |
+| `sql-query`                              | SQL query                                                                                                                                   |
+| `materialized-view-dataland-table-name`  | Output table name in Dataland                                                                                                               |
+| `materialized-view-identity-column-name` | Primary key column for the join. Composite keys can be used as well, but must be specified as `"{column1}", "{column2}", "{column3}"`, etc. |
+| `keep-extra-columns-boolean`             | This preserves any extra columns added to the output table in the Dataland UI.                                                              |
---
+++ materialized-view/src/materializedView.ts
@@ -0,0 +1,66 @@
+import {
+  querySqlMirror,
+  registerCronHandler,
+  syncTables,
+  SyncTable,
+  getEnv,
+} from "@dataland-io/dataland-sdk-worker";
+
+const sqlQuery = getEnv("SQL_QUERY");
+if (sqlQuery == null) {
+  throw new Error("Missing environment variable - SQL_QUERY");
+}
+const materializedViewDatalandTableName = getEnv(
+  "MATERIALIZED_VIEW_DATALAND_TABLE_NAME"
+);
+if (materializedViewDatalandTableName == null) {
+  throw new Error(
+    "Missing environment variable - MATERIALIZED_VIEW_DATALAND_TABLE_NAME"
+  );
+}
+const materializedViewIdentityColumnName = getEnv(
+  "MATERIALIZED_VIEW_IDENTITY_COLUMN_NAME"
+);
+if (materializedViewIdentityColumnName == null) {
+  throw new Error(
+    "Missing environment variable - MATERIALIZED_VIEW_IDENTITY_COLUMN_NAME"
+  );
+}
+const keepExtraColumns = getEnv("KEEP_EXTRA_COLUMNS_BOOLEAN");
+if (keepExtraColumns == null) {
+  throw new Error("Missing environment variable - KEEP_EXTRA_COLUMNS_BOOLEAN");
+}
+
+let keepExtraColumnsBoolean = true;
+
+if (keepExtraColumns === "false") {
+  keepExtraColumnsBoolean = false;
+}
+
+const handler = async () => {
+  // Construct the new join query
+  const joined_query = await querySqlMirror({
+    sqlQuery: `${sqlQuery}`,
+  });
+
+  if (joined_query == null) {
+    return;
+  }
+
+  const syncTable: SyncTable = {
+    tableName: `${materializedViewDatalandTableName}`,
+    arrowRecordBatches: joined_query.arrowRecordBatches,
+    identityColumnNames: [`${materializedViewIdentityColumnName}`],
+    keepExtraColumns: keepExtraColumnsBoolean,
+  };
+  try {
+    await syncTables({
+      syncTables: [syncTable],
+    });
+    console.log("Sync complete");
+  } catch (e) {
+    console.warn(`syncTables failed`, e);
+  }
+};
+
+registerCronHandler(handler);
