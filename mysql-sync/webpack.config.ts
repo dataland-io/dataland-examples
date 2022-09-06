428ea074ffcb6b96ea0f02db41d3c4c169f0da6d
Arthur Wu <arthur@realismlabs.com>
2022-09-06 14:59:39 -0400

    Add MySQL module (#29)

---
+++ mysql-sync/webpack.config.ts
@@ -0,0 +1,39 @@
+import * as path from "path";
+import type { Configuration } from "webpack";
+
+const config: Configuration = {
+  mode: "production",
+  target: "web",
+  entry: {
+    mysqlSync: "./src/mysqlSync.ts",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+    clean: true,
+  },
+  performance: {
+    maxAssetSize: 10_000_000,
+    maxEntrypointSize: 10_000_000,
+  },
+  optimization: {
+    // for pretty stacktraces
+    minimize: false,
+  },
+  experiments: {
+    futureDefaults: true,
+  },
+};
+
+export default config;
